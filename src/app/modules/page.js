d828aa254406ea2adc0b513411d24351f8098d27
Noel K <nbkurian11@gmail.com>
2026-04-25 20:00:45 -0400

    initial setup

---
+++ src/app/modules/page.js
@@ -0,0 +1,10 @@
+export default function Modules() {
+    return (
+        <main className = "container">
+            <h1>Modules</h1>
+            <ul>Classes and Objects</ul>
+            <ul>this keyword & Constructors</ul>
+            <ul>Inheritance</ul>
+        </main>
+    )
+}
