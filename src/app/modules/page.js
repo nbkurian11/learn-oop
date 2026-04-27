a291d335606aa195071162769a4b97aa44a68706
Noel K <nbkurian11@gmail.com>
2026-04-26 21:42:44 -0400

    added modules list

--- src/app/modules/page.js
+++ src/app/modules/page.js
@@ -1,10 +1,29 @@
 export default function Modules() {
-    return (
+const modules = [
+  "Classes and Objects",
+  "Constructors and the this Keyword",
+  "Encapsulation",
+  "Static Methods and Variables",
+  "Inheritance",
+  "Method Overriding and Polymorphism",
+  "Abstract Classes",
+  "Interfaces",
+  "Exception Handling",
+  "Recursion",
+  "Searching and Sorting",
+  "Stacks and Queues",
+  "Linked Lists",
+  "Binary Trees",
+  "Big-O Notation",
+]
+return (
         <main className = "container">
             <h1>Modules</h1>
-            <ul>Classes and Objects</ul>
-            <ul>this keyword & Constructors</ul>
-            <ul>Inheritance</ul>
+            <ul>
+                {modules.map((topic) => {
+                    return <li key={topic}>{topic}</li>
+                })}
+            </ul>
         </main>
     )
 }
